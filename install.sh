e6fae25e01bea5741c2c491f6c2d0a3515862c80
Roy E <royevangelista@gmail.com>
2024-10-14 19:12:40 +0800

    Changes for Neovim

--- install.sh
+++ install.sh
@@ -8,8 +8,9 @@ if [[ "$OSTYPE" == "linux-gnu"*  ]]; then
     sudo apt update
 
     # Install required system libraries
-    sudo apt install vim vim-gtk3 tmux git curl build-essential cmake python3-dev
+    sudo apt install neovim tmux git curl build-essential cmake python3-dev
 elif [[ "$OSTYPE" == "darwin"*  ]]; then
+    brew install neovim
     brew install tmux
     brew install reattach-to-user-namespace
 else
@@ -17,18 +18,23 @@ else
     exit 1
 fi
 
+mkdir -p ~/.config/nvim
+
 # Install vim-plug
-curl -fLo ~/.vim/autoload/plug.vim \
+# curl -fLo ~/.vim/autoload/plug.vim \
+#     --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo ~/.config/nvim/autoload/plug.vim \
     --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # Create a symlink for .vimrc file
-ln -s ~/dotfiles/vimrc ~/.vimrc
-ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
+# ln -s ~/dotfiles/vimrc ~/.vimrc
+ln -s -F ~/dotfiles/vimrc ~/.config/nvim/init.vim
+ln -s -F ~/dotfiles/tmux.conf ~/.tmux.conf
 
 # Install tmux theme.
 git clone https://github.com/jimeh/tmux-themepack.git ~/.tmux-themepack
 
-vim +PlugInstall +qall
+nvim +PlugInstall +qall
 
 # cd ~/.vim/plugged/YouCompleteMe/
 # python3 install.py --clangd-completer --go-completer --rust-completer
